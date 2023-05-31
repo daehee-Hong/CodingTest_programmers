bf173d8e2f793c1b49b35fa97b63088462a13568
daehee-Hong <91270516+daehee-Hong@users.noreply.github.com>
2023-05-31 21:24:30 +0900

    [level 0] Title: 7의 개수, Time: 24.59 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -1,16 +1,17 @@
-import java.util.*;
-import java.util.stream.Collectors;
-
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
 
-        String collect = Arrays.stream(array).mapToObj(v -> String.valueOf(v)).collect(Collectors.joining());
-        for (String s : collect.split("")) {
-            if (s.equals("7")){
-                answer++;
-            }
+        String str = "";
+
+        for(int i=0; i<array.length; i++){
+            str += array[i];
         }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '7') answer++;
+        }
+
         return answer;
     }
 }
